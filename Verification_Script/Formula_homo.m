0853cf6ced977613e1df7ccb00cfe8c18f9c1197
Xin Zhou <houxin0714@gmail.com>
2021-02-20 00:09:41 +0100

    精简代码

--- Verification_Script/Formula_homo.m
+++ Verification_Script/Formula_homo.m
@@ -12,71 +12,77 @@ syms vz [1 6]  %%% link displacement in z direction
 
 syms L [1 2]  %%% link length for 3DOF
 %%%% Rx
-Rx1_1(alpha)=1;
-Rx2_1(alpha)=0;
-Rx3_1(alpha)=0;
-Rx4_1(alpha)=0;
+Rx= @(x) trotx(x);
 
-Rx1_2(alpha)=0;
-Rx2_2(alpha)=cos(alpha);
-Rx3_2(alpha)=sin(alpha);
-Rx4_2(alpha)=0;
-
-Rx1_3(alpha)=0;
-Rx2_3(alpha)=-sin(alpha);
-Rx3_3(alpha)=cos(alpha);
-Rx4_3(alpha)=0;
-
-Rx1_4(alpha)=0;
-Rx2_4(alpha)=0;
-Rx3_4(alpha)=0;
-Rx4_4(alpha)=1;
+% Rx1_1(alpha)=1;
+% Rx2_1(alpha)=0;
+% Rx3_1(alpha)=0;
+% Rx4_1(alpha)=0;
+% 
+% Rx1_2(alpha)=0;
+% Rx2_2(alpha)=cos(alpha);
+% Rx3_2(alpha)=sin(alpha);
+% Rx4_2(alpha)=0;
+% 
+% Rx1_3(alpha)=0;
+% Rx2_3(alpha)=-sin(alpha);
+% Rx3_3(alpha)=cos(alpha);
+% Rx4_3(alpha)=0;
+% 
+% Rx1_4(alpha)=0;
+% Rx2_4(alpha)=0;
+% Rx3_4(alpha)=0;
+% Rx4_4(alpha)=1;
 
 %%%% Ry
-Ry1_1(beta)=cos(beta);
-Ry2_1(beta)=0;
-Ry3_1(beta)=-sin(beta);
-Ry4_1(beta)=0;
+Ry= @(x) troty(x);
 
-Ry1_2(beta)=0;
-Ry2_2(beta)=1;
-Ry3_2(beta)=0;
-Ry4_2(beta)=0;
-
-Ry1_3(beta)=sin(beta);
-Ry2_3(beta)=0;
-Ry3_3(beta)=cos(beta);
-Ry4_3(beta)=0;
-
-Ry1_4(beta)=0;
-Ry2_4(beta)=0;
-Ry3_4(beta)=0;
-Ry4_4(beta)=1;
+% Ry1_1(beta)=cos(beta);
+% Ry2_1(beta)=0;
+% Ry3_1(beta)=-sin(beta);
+% Ry4_1(beta)=0;
+% 
+% Ry1_2(beta)=0;
+% Ry2_2(beta)=1;
+% Ry3_2(beta)=0;
+% Ry4_2(beta)=0;
+% 
+% Ry1_3(beta)=sin(beta);
+% Ry2_3(beta)=0;
+% Ry3_3(beta)=cos(beta);
+% Ry4_3(beta)=0;
+% 
+% Ry1_4(beta)=0;
+% Ry2_4(beta)=0;
+% Ry3_4(beta)=0;
+% Ry4_4(beta)=1;
 
 
 %%%% Rz
-Rz1_1(gamma)=cos(gamma);
-Rz2_1(gamma)=sin(gamma);
-Rz3_1(gamma)=0;
-Rz4_1(gamma)=0;
+Rz= @(x) trotz(x);
 
-Rz1_2(gamma)=-sin(gamma);
-Rz2_2(gamma)=cos(gamma);
-Rz3_2(gamma)=0;
-Rz4_2(gamma)=0;
-
-Rz1_3(gamma)=0;
-Rz2_3(gamma)=0;
-Rz3_3(gamma)=1;
-Rz4_3(gamma)=0;
-
-Rz1_4(gamma)=0;
-Rz2_4(gamma)=0;
-Rz3_4(gamma)=0;
-Rz4_4(gamma)=1;
+% Rz1_1(gamma)=cos(gamma);
+% Rz2_1(gamma)=sin(gamma);
+% Rz3_1(gamma)=0;
+% Rz4_1(gamma)=0;
+% 
+% Rz1_2(gamma)=-sin(gamma);
+% Rz2_2(gamma)=cos(gamma);
+% Rz3_2(gamma)=0;
+% Rz4_2(gamma)=0;
+% 
+% Rz1_3(gamma)=0;
+% Rz2_3(gamma)=0;
+% Rz3_3(gamma)=1;
+% Rz4_3(gamma)=0;
+% 
+% Rz1_4(gamma)=0;
+% Rz2_4(gamma)=0;
+% Rz3_4(gamma)=0;
+% Rz4_4(gamma)=1;
 
 %%%%Pure rotation
-subs(Rx(alpha)* Ry(beta)*Rz(gamma+q));
+subs(Rx(alpha)* Ry(beta)*Rz(gamma));
 
 %%%% translation+ rotation
 
