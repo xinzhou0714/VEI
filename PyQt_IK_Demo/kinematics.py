9fc5d94e41a86b22e37139384eb35577347e9e35
Xin Zhou <houxin0714@gmail.com>
2021-03-13 22:38:33 +0100

    input control for theta

--- PyQt_IK_Demo/kinematics.py
+++ PyQt_IK_Demo/kinematics.py
@@ -14,6 +14,10 @@ class Kinematics(object):
 
     @theta.setter
     def theta(self, value):
+        if len(value)!=8:
+            raise ValueError(" theta must be array with length 8")
+        if value[0]!=0 or  value[-1]!=0 :
+            raise ValueError(" the  angle of first and  last joint must be  zero   ")
         print("set theta,old value is: %s" % self._theta)
         print("set theta,new value is: %s" % value)
         self._theta=value
@@ -25,5 +29,5 @@ class Kinematics(object):
 
 if __name__ == '__main__':
     kn=Kinematics()
-    kn.theta=np.ones(8)
+    kn.theta=np.array([0,0,20,0,0,0,0,0],dtype=float)
     
