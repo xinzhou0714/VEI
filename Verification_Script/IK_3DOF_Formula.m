4222ad182b2b7c0a8225d03ed8310365689cbaff
Xin Zhou <houxin0714@gmail.com>
2021-03-21 20:25:10 +0100

    next commmit

--- Verification_Script/IK_3DOF_Formula.m
+++ Verification_Script/IK_3DOF_Formula.m
@@ -13,15 +13,17 @@ T2ee=transl(L2,0,0)*trotz(q3);
 
 Tb1=simplify(Tb1);
 Tb2=simplify(Tb1*T12);
-Tbee=simplify(Tb1*T12*T2ee);
+Tbee=simplify(Tb1*T12*T2ee)
 
-x(q1,q2,q3)=Tbee(1,4)
-y(q1,q2,q3)=Tbee(2,4)
-z(q1,q2,q3)=Tbee(3,4)
+x(H, L1, L2,q1,q2,q3)=Tbee(1,4)
+y(H, L1, L2,q1,q2,q3)=Tbee(2,4)
+z(H, L1, L2,q1,q2,q3)=Tbee(3,4)
 
 Rb1=Tb1(1:3,1:3)
 Rb2=Tb2(1:3,1:3)
 Rbee=Tbee(1:3,1:3)
+
+Rbeefun(q1,q2,q3)=Tbee(1:3,1:3)
 
 omega_b1=Rb1*[0;0;1]
 omega_b2=Rb2*[0;0;1]
@@ -32,4 +34,4 @@ J_down=[omega_b1,omega_b2,omega_bee]
 
 Jm=[J_up;J_down]
 
-T_res(p1, p2 ,p3, r1, r2 ,r3)=transl(p1,p2,p3)*trotx(r1)*troty(r2)*trotz(r3)
+%T_res(p1, p2 ,p3, r1, r2 ,r3)=transl(p1,p2,p3)*trotx(r1)*troty(r2)*trotz(r3)
