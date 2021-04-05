a78842c4bd6a7e2399e69e5e3093102abe612f2f
Xin Zhou <houxin0714@gmail.com>
2021-04-05 11:36:14 +0200

    modify comments

--- Verification_Script/T_adjacent.m
+++ Verification_Script/T_adjacent.m
@@ -1,12 +1,9 @@
 function T = T_adjacent(OX	 ,OY    ,OZ    ,PX    ,PY	 ,PZ)
+%   PX: relativ offset along old x axis
+%   PY: relativ offset along old y axis
+%   PZ: relativ offset along old z axis
 %   OX:rotation around old x axis
 %   OY:rotation around old y axis
 %   OZ:rotation around old z axis
-%   PX: relativ offset along new x axis
-%   PY: relativ offset along new y axis
-%   PZ: relativ offset along new z axis
-
-
 T =transl(PX,PY,PZ)*trotx(OX) *troty(OY)*trotz(OZ);
-
 end
